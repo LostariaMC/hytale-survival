efbc289dc5a880f79e21fec9d826903af4f7f52e
Erpriex <erpriex@gmail.com>
2026-01-16 17:49:52 +0100

    ⚡️ Switch broadcast cmd to abstract player command

--- src/main/java/fr/lostaria/hytalesurvival/commands/BroadcastCommand.java
+++ src/main/java/fr/lostaria/hytalesurvival/commands/BroadcastCommand.java
@@ -1,18 +1,23 @@
 package fr.lostaria.hytalesurvival.commands;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.EventTitleUtil;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 import java.awt.*;
 
-public class BroadcastCommand extends CommandBase {
+public class BroadcastCommand extends AbstractPlayerCommand {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
@@ -22,20 +27,19 @@ public class BroadcastCommand extends CommandBase {
         setAllowsExtraArguments(true);
     }
 
+
     @Override
-    protected void executeSync(@NonNullDecl CommandContext commandContext) {
-        if(!commandContext.isPlayer()) return;
-        Player player = commandContext.senderAs(Player.class);
-        String input = commandContext.getInputString();
+    protected void execute(@NonNullDecl CommandContext context, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
+        String input = context.getInputString();
         String message = input.replaceFirst("^\\S+\\s*", "").trim();
         if(message.isEmpty()) {
-            player.sendMessage(Message.raw("Vous ne pouvez pas envoyer une annonce vide !\nUsage : /broadcast <message>").color(Color.RED));
+            playerRef.sendMessage(Message.raw("Vous ne pouvez pas envoyer une annonce vide !\nUsage : /broadcast <message>").color(Color.RED));
             return;
         }
         LOGGER.atInfo().log("Broadcasting '" + message + "' to all players");
         Universe universe = Universe.get();
         for(PlayerRef plsR : universe.getPlayers()) {
-            EventTitleUtil.showEventTitleToPlayer(plsR, Message.raw(message), Message.raw("ANNONCE DE " + player.getDisplayName()), true, null, 10f, 0.1f, 0.1f);
+            EventTitleUtil.showEventTitleToPlayer(plsR, Message.raw(message), Message.raw("ANNONCE DE " + playerRef.getUsername()), true, null, 10f, 0.1f, 0.1f);
         }
     }
 }
